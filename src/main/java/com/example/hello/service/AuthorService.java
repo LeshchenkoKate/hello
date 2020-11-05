5bb370bbf01f37ad2ededfa23920b5754412fbf6
LeshchenkoKate <34144754+LeshchenkoKate@users.noreply.github.com>
2020-11-05 16:09:56 +0200

    Update AuthorService.java

--- src/main/java/com/example/hello/service/AuthorService.java
+++ src/main/java/com/example/hello/service/AuthorService.java
@@ -2,7 +2,7 @@ package com.example.hello.service;
 
 import com.example.hello.model.Author;
 import org.springframework.stereotype.Service;
-
+//add
 @Service
 public interface AuthorService {
     void addAuthor(Author author);
