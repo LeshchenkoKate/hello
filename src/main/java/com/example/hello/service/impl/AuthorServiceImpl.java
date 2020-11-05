83d8c54779ae9ab27b3dda590530507deb64b8f5
LeshchenkoKate <34144754+LeshchenkoKate@users.noreply.github.com>
2020-11-05 16:10:43 +0200

    Update AuthorServiceImpl.java

--- src/main/java/com/example/hello/service/impl/AuthorServiceImpl.java
+++ src/main/java/com/example/hello/service/impl/AuthorServiceImpl.java
@@ -5,7 +5,7 @@ import com.example.hello.repsitory.AuthorRepository;
 import com.example.hello.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+//add
 @Service
 public class AuthorServiceImpl implements AuthorService {
     @Autowired
